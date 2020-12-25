2c512f5b14c0236c97bfc931283a5a2d47c5e98e
Newyorkallen <52312258+Newyorkallen@users.noreply.github.com>
2020-12-25 23:55:38 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function showMenu(){
+	let menu=document.querySelector("#mobile-menu");
+	menu.style.display="block";
+}
+function closeMenu(){
+	let menu=document.querySelector("#mobile-menu");
+	menu.style.display="none";
+}
